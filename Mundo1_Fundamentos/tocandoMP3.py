900f0d8ee75bafb8651581e083540f9f5c3d4f18
ateixeiraleal <andersonpains@yahoo.com.br>
2020-11-26 14:05:09 -0300

    Correção no arquivo tocandoMP3.py

--- Mundo1_Fundamentos/tocandoMP3.py
+++ Mundo1_Fundamentos/tocandoMP3.py
@@ -1,5 +1,26 @@
+# Importando o PyGame
 import pygame
-pygame.init() #inicializa o pygame.
-pygame.mixer.music.load('sextou.mp3') #define a música a ser tocada.
-pygame.mixer.music.play() #executa o arquivo de música.
-pygame.event.wait() #espera o evento terminar para encerrar o programa.
+import os
+
+# Inicializando o Mixer do PyGame
+pygame.mixer.init()
+
+# Gerando a lista de músicas
+print('\033[1;36m{} MÚSICAS DISPONÍVEIS {}'.format('-=-'*5, '-=-'*5))
+for c in os.listdir():
+    if '.wav' in c:
+        print(c)
+print('-=-'*17, '\033[m')
+
+musica = input('Entre com o nome da música: ')
+
+# Carregando o arquivo MP3 e executando
+if os.path.exists(musica):
+    pygame.mixer.music.load(musica) #define a música a ser tocada.
+    pygame.mixer.music.play() #executa o arquivo de música.
+    pygame.mixer.music.set_volume(1)
+    input('Digite para parar')
+else:
+    print('\033[1;31mPossíveis erros:')
+    print('>>> O arquivo {} não está no diretório do script Python'.format(musica))
+    print('>>> Formato do arquivo não foi informado ".wav"')
